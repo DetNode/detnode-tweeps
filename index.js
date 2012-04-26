36413f94d724653b3fb68ee74a4d35053f5dd2d9
Kevin Swiber <kswiber@gmail.com>
2012-04-25 21:24:09 -0400

    Adding colors to Twitter names, rainbowizing kevino80. ;P

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 var Twitter = require('ntwitter');
 
-var twit = new Twitter({
+var twitter = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-twit.search('#nodejs', { include_entities: true }, function(err, data) {
-  console.log(data);
+
+twitter.search('#nodejs OR nodejs OR node.js', { geocode: '42.3319,-83.046513,60mi', rpp: 100, request_type: 'recent' }, function(err, data) {
+  data.results.forEach(function(tweet) {
+    console.log(stylize(tweet.from_user) + ':', tweet.text);
+  });
 });
+
+
+var colorIndex = 0;
+function stylize(str) {
+  if (str === 'kevino80') {
+    return rainbowize(str);
+  }
+
+  var colors = ['cyan', 'magenta', 'yellow', 'red', 'green', 'blue', ];
+  colorIndex = (colorIndex == colors.length - 1) ? 0 : colorIndex + 1;
+  return wrapStyle(str, colors[colorIndex]);
+}
+
+function rainbowize(str) {
+  var colors = ['red', 'yellow', 'green', 'blue'];
+  var rainbow = '';
+  var index = 0;
+  str.split('').forEach(function(s) {
+    rainbow += wrapStyle(s, colors[index]);
+    index = (index == colors.length - 1) ? 0 : index + 1;
+  });
+
+  return rainbow;
+}
+
+function wrapStyle(str, styles) {
+  if (typeof styles == 'string') {
+    styles = [styles];
+  }
+
+  var allStyles = {
+    'blue': ['\033[34m', '\033[39m'],
+    'cyan': ['\033[36m', '\033[39m'],
+    'green': ['\033[32m', '\033[39m'],
+    'magenta': ['\033[35m', '\033[39m'],
+    'red': ['\033[31m', '\033[39m'],
+    'yellow': ['\033[33m', '\033[39m'],
+  };
+
+  function addStyle(str, style) {
+    return style[0] + str + style[1];
+  }
+
+  var stylized = str;
+  styles.forEach(function(style) {
+    stylized = addStyle(stylized, allStyles[style]);
+  });
+
+  return stylized;
+}
